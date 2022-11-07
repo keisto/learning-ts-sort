927c37ecf020b5442cb4411001b8e60a58f87e30
Tony Keiser <keisertony@gmail.com>
2022-11-07 00:54:50 +0000

    Added sortable interface to allow for sorting strings

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter'
+import { CharactersCollection } from './CharactersCollection'
 import { NumbersCollection } from './NumbersCollection'
 
 const numCollection = new NumbersCollection([10, 3, -5, 0])
-const sorter = new Sorter(numCollection)
-sorter.sort()
+const numSorter = new Sorter(numCollection)
+numSorter.sort()
 console.log(numCollection.data)
+
+const charCollection = new CharactersCollection('TonyK')
+const charSorter = new Sorter(charCollection)
+charSorter.sort()
+console.log(charCollection.data)

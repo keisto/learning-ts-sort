e1d10b5231d7f60e98dc6bd4a392fc6a10aed606
Tony Keiser <keisertony@gmail.com>
2022-11-07 06:21:50 +0000

    Added LinkedList class with sorting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Sorter } from './Sorter'
 import { CharactersCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedList'
 import { NumbersCollection } from './NumbersCollection'
 
 const numCollection = new NumbersCollection([10, 3, -5, 0])
@@ -11,3 +12,12 @@ const charCollection = new CharactersCollection('TonyK')
 const charSorter = new Sorter(charCollection)
 charSorter.sort()
 console.log(charCollection.data)
+
+const linkedlist = new LinkedList()
+linkedlist.add(500)
+linkedlist.add(-10)
+linkedlist.add(-32)
+linkedlist.add(40)
+const linkedListSorter = new Sorter(linkedlist)
+linkedListSorter.sort()
+linkedlist.print()

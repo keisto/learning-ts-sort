4e80fdacf7ca4bd45e928812a132c8d3133743fe
Tony Keiser <keisertony@gmail.com>
2022-11-07 00:39:08 +0000

    Refactored sorter to be more dynamic

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,7 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { Sorter } from './Sorter'
+import { NumbersCollection } from './NumbersCollection'
 
-  sort(): void {
-    const { length } = this.collection
-
-    // Bubble Sort
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j]
-          this.collection[j] = this.collection[j + 1]
-          this.collection[j + 1] = leftHand
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0])
+const numCollection = new NumbersCollection([10, 3, -5, 0])
+const sorter = new Sorter(numCollection)
 sorter.sort()
-console.log(sorter.collection)
+console.log(numCollection.data)
